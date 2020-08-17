2240068d0d19b207317294666a2d48d2bf072694
George Liu <georgeliu220@gmail.com>
2020-08-17 12:35:44 -0400

    add external mongo atlas db

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect("mongodb+srv://admin-george:test-123@cluster0.silqc.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 const itemsSchema = {
   name: String
